c1bc82c9b8c853bd9afc926276c6ddac24fa5ea0
iorgaandreivasile <48823834+iorgaandreivasile@users.noreply.github.com>
2019-04-21 23:52:46 +0300

    Lab2

---
+++ Laborator2/src/L2/StatisticsUtils.java
@@ -0,0 +1,88 @@
+package L2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatisticsUtils 
+{
+
+	protected static double calculateFeatureAverage(Double[] feature) {
+		Map<Double, Integer> counterMap = getFeatureDistincElementsCounterMap(feature);
+		double featureAverage = 0;
+
+		double sum1 = 0;
+		double sum2 = 0;
+
+		sum1 = counterMap.keySet().stream()
+				.mapToDouble(x -> calculateSum1(x, counterMap.get(x))).sum();
+		sum2 = counterMap.values().stream()
+				.mapToInt(x -> x).sum();
+		featureAverage = sum1 / sum2;
+		System.out.println("The feature average is............: " +  featureAverage);
+		return featureAverage;
+}
+	
+	protected static Map<Double, Integer> getFeatureDistincElementsCounterMap(Double feature[])
+	{
+		Map<Double, Integer> counterMap = new HashMap<Double, Integer>();
+		for (int j = 0; j < feature.length; j++) {
+			if (counterMap.containsKey(feature[j])) {
+				int count = counterMap.get(feature[j]);
+				counterMap.put((feature[j]), ++count);
+			} else {
+				counterMap.put((feature[j]), 1);
+			}
+		}
+		return counterMap;
+	}
+	
+	private static Double calculateSum1(double value, int count)
+	{
+		return count * value;
+	}
+
+	protected static double calculateFeatureWeightedAverage(Double[] feature, Double[] weights) {
+		double featureWeightedAverage = 0.0;
+		double sum1 = 0;
+		double sum2 = 0;
+		for(int i=0; i<feature.length; i++)
+		{
+			sum1 +=(feature[i] * weights[i]);
+			sum2 += weights[i];
+		}
+		featureWeightedAverage = sum1/sum2;
+		return featureWeightedAverage;
+	}
+	
+	protected static double calculateFrequencyOfOccurence(Map<Double, Integer> counterMap, double featureElement) {
+		double frequencyOfOccurence = counterMap.get(featureElement)/(double)counterMap.values().stream().mapToInt(x -> x).sum();
+		
+		return frequencyOfOccurence;
+	}
+	
+	protected static double calculateFeatureDispersion(Double[] feature, double featureWeightedAverage) {
+		double featureDispersion = 0.0;
+		// your code here
+		return featureDispersion;
+	}
+	
+	protected static double calculateCovariance (Double[] feature1, Double[] feature2,
+			double feature1WeightedAverage,double feature2WeightedAverage) {
+		double covariance = 0.0;
+		// your code here
+		return covariance;
+	}
+	
+	protected static double calculateCorrelationCoefficient  (double covariance, double feature1Dispersion, 
+			double feature2Dispersion ) {
+		double correlationCoefficient = 0.0;
+		// your code here
+		return correlationCoefficient;
+	}
+	
+	protected static double calculateAverageSquareDeviation (double featureDispersion ) {
+		double averageSquareDeviation = 0.0;
+		// your code here
+		return averageSquareDeviation;
+	}
+}
